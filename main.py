ed2b26232d10c5e53c998a0cde6a40d624613537
Niall Grainger <niallgrainger@gmail.com>
2022-10-18 20:42:41 +0100

    Remove imports

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-import requests
 import time
 
 columnslist = ["Source", "Firm", "URL", "Email_Address"]
